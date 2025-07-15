ccc09620626d5d9b704cb81b30691e241b66782d
joranschaefer <100149107+joranschaefer@users.noreply.github.com>
2025-07-15 15:12:19 +0200

    Update PlayerUpdates.cpp

--- src/server/game/Entities/Player/PlayerUpdates.cpp
+++ src/server/game/Entities/Player/PlayerUpdates.cpp
@@ -1681,6 +1681,8 @@ template <class T>
 void Player::UpdateVisibilityOf(T* target, UpdateData& data,
                                 std::vector<Unit*>& visibleNow)
 {
+    GetMap()->AddObjectToPendingUpdateList(target);
+    
     if (HaveAtClient(target))
     {
         if (!CanSeeOrDetect(target, false, true))
